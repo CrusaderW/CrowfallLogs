47e2cd887eee90f14936346a49e0c767e02fa306
Arc_Anya <arcanyatwitch@gmail.com>
2021-01-15 11:30:20 +0100

    separate CharacterSelectionBar from CharacterPlanner component

---
+++ src/views/CharacterSelectionRoute.vue
@@ -0,0 +1,14 @@
+<template>
+  <character-selection-bar />
+</template>
+
+<script>
+import CharacterSelectionBar from '@/components/CharacterSelectionBar.vue';
+
+export default {
+  name: "Home",
+  components: {
+    CharacterSelectionBar
+  },
+};
+</script>
