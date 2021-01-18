1a7f893a8e75414df9b3776f10b955d296f21eb1
Arc_Anya <arcanyatwitch@gmail.com>
2021-01-18 09:51:13 +0100

    add input table for attributes

--- src/components/CharacterPlannerAttributes.vue
+++ src/components/CharacterPlannerAttributes.vue
@@ -1,7 +1,37 @@
 <template>
   <div id="root">
-    I'm the attributes part!
+    <div class="title">Attributes</div>
+    <div>
+      <table>
+        <tr>
+            <td>STR</td>
+            <td><input type="text" value="Current"></td>
+            <td><input type="text" value="Cap"></td>
+        </tr>
+        <tr>
+            <td>DEX</td>
+            <td><input type="text" value="Current"></td>
+            <td><input type="text" value="Cap"></td>
+        </tr>
+        <tr>
+            <td>INT</td>
+            <td><input type="text" value="Current"></td>
+            <td><input type="text" value="Cap"></td>
+        </tr>
+        <tr>
+            <td>SPR</td>
+            <td><input type="text" value="Current"></td>
+            <td><input type="text" value="Cap"></td>
+        </tr>
+        <tr>
+            <td>CON</td>
+            <td><input type="text" value="Current"></td>
+            <td><input type="text" value="Cap"></td>
+        </tr>
+    </table>
+    </div>
   </div>
+
 </template>
 
 
@@ -16,5 +46,13 @@ export default {
 #root {
     background-color: saddlebrown;
     color: white;
+    max-width: 11em;
+}
+.title {
+  background-color: chocolate;
+}
+input {
+  background-color: crimson;
+  max-width: 4em;
 }
 </style> 
