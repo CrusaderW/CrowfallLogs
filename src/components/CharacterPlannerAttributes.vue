658850868a8ba9a93d22f3d16daa7a3c387877db
Arc_Anya <arcanyatwitch@gmail.com>
2021-01-18 12:15:57 +0100

    add slide effect to Attibutes

--- src/components/CharacterPlannerAttributes.vue
+++ src/components/CharacterPlannerAttributes.vue
@@ -1,58 +1,93 @@
 <template>
-  <div id="root">
-    <div class="title">Attributes</div>
-    <div>
-      <table>
-        <tr>
+  <div id="root1">
+    <transition name="slide">
+      <div v-if="isOpen">
+        <div class="title">Attributes</div>
+        <table>
+          <tr>
             <td>STR</td>
-            <td><input type="text" value="Current"></td>
-            <td><input type="text" value="Cap"></td>
-        </tr>
-        <tr>
+            <td><input type="text" value="Current" /></td>
+            <td><input type="text" value="Cap" /></td>
+          </tr>
+          <tr>
             <td>DEX</td>
-            <td><input type="text" value="Current"></td>
-            <td><input type="text" value="Cap"></td>
-        </tr>
-        <tr>
+            <td><input type="text" value="Current" /></td>
+            <td><input type="text" value="Cap" /></td>
+          </tr>
+          <tr>
             <td>INT</td>
-            <td><input type="text" value="Current"></td>
-            <td><input type="text" value="Cap"></td>
-        </tr>
-        <tr>
+            <td><input type="text" value="Current" /></td>
+            <td><input type="text" value="Cap" /></td>
+          </tr>
+          <tr>
             <td>SPR</td>
-            <td><input type="text" value="Current"></td>
-            <td><input type="text" value="Cap"></td>
-        </tr>
-        <tr>
+            <td><input type="text" value="Current" /></td>
+            <td><input type="text" value="Cap" /></td>
+          </tr>
+          <tr>
             <td>CON</td>
-            <td><input type="text" value="Current"></td>
-            <td><input type="text" value="Cap"></td>
-        </tr>
-    </table>
-    </div>
+            <td><input type="text" value="Current" /></td>
+            <td><input type="text" value="Cap" /></td>
+          </tr>
+        </table>
+      </div>
+    </transition>
+    <button @click="pressSlide()">{{ buttonLook }}</button>
   </div>
-
 </template>
 
 
 <script>
 export default {
   name: "CharacterPlannerAttributes",
-  data() {},
+  data() {
+    return {
+      isOpen: true,
+      buttonLook: "<<",
+    };
+  },
+  methods: {
+    pressSlide() {
+      this.isOpen = !this.isOpen;
+      if (this.isOpen) {
+        this.buttonLook = "<<";
+      } else {
+        this.buttonLook = ">>";
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-#root {
-    background-color: saddlebrown;
-    color: white;
-    max-width: 11em;
+#root1 {
+  display: flex;
+  background-color: saddlebrown;
+  color: white;
+  flex-basis: 12em;
+  overflow: hidden;
+}
+.slide-leave-active,
+.slide-enter-active {
+  transition: 1s;
+}
+.slide-leave-to,
+.slide-enter-from {
+  transform: translate(-100%, 0);
 }
 .title {
   background-color: chocolate;
+  padding: 0.5em;
 }
 input {
   background-color: crimson;
   max-width: 4em;
+}
+table {
+  padding: 0.5em;
+}
+button {
+  background-color: orange;
+  flex-basis: 3em;
 }
 </style> 
