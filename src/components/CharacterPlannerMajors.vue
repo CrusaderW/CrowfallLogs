046e23a333729224d6158758fc05bbb3eb3bcfe2
Arc_Anya <arcanyatwitch@gmail.com>
2021-02-09 10:18:31 +0100

    filtering majors depending on selected class

--- src/components/CharacterPlannerMajors.vue
+++ src/components/CharacterPlannerMajors.vue
@@ -7,20 +7,22 @@
     </div>
     <div>
       <select v-model="selectedDiscipline">
-        <option disabled value="">Choose Major </option>
-        <option v-for="discipline in data.majors_list" :key="discipline.majorname">
+        <option disabled value="">Choose Major</option>
+        <option
+          v-for="discipline in data.majors_list"
+          :key="discipline.majorname"
+        >
           {{ discipline.majorname }}
         </option>
       </select>
     </div>
-    <div>{{ data.majors_list["Adjudicator"]["classes_possible"] }}</div>
-    <h3>{{ finalClass }} - {{ finalRace }} - {{ finalDomain }}</h3> <!-- BEST PRACTICE: don't get data directly from state but from getters -->
+    <div>{{ findClasses() }}</div>
   </div>
 </template>
 
 <script>
 import data from "../../data/CPD_MinorsMajors.json";
-import {mapGetters} from 'vuex'; // instead of using a computed function that only does: "return this.$store.getters.finalClass;"
+import { mapGetters } from "vuex"; // instead of using a computed function that only does: "return this.$store.getters.finalClass;"
 //we can use Mapper Helpers (in this case mapGetters) to receive getters from the store
 
 export default {
@@ -29,17 +31,39 @@ export default {
     return {
       data,
       selectedDiscipline: "",
+      filteredDisciplines: [],
     };
   },
+  watch: {
+    finalClass(newClass, oldClass) {
+      if (newClass !== oldClass) {
+        this.filteredDisciplines = [];
+      }
+    }
+  },
   computed: {
-    /* showClass() { // this function is there to get data from store via getter
-      return this.$store.getters.finalClass; // finalClass is the getter defined in vuex store
-    }, */
-    ...mapGetters('charPlanner', ['finalClass', 'finalRace', 'finalDomain']) 
+    /* testData() {
+          if(this.finalClass !== "") {
+              return this.filteredDisciplines;
+          }
+          return "Yiha";
+      }, */
+    ...mapGetters("charPlanner", ["finalClass", "finalRace", "finalDomain"]),
     // Note 1: can input the getters in an array list if need more than one
     // Note 2: when you have 'namespaced' a module in the store, you need to first refer to this module ('charPlanner') and only then can you get the data
   },
   methods: {
+    findClasses() {
+      if (this.finalClass !== "") {
+        var discipline;
+        for (discipline in this.data.majors_list) {
+          if (this.data.majors_list[discipline]["classes_possible"].indexOf(this.finalClass) >= 0) {
+            this.filteredDisciplines.push(this.data.majors_list[discipline]["majorname"]);
+          }
+        }
+      }
+      return this.filteredDisciplines;
+    },
   },
 };
 </script>
